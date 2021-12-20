7e1c9fc911e1487deed2173f5e83561529d0a44e
m1chaeldg <nonoymsd@gmail.com>
2021-12-20 10:19:47 -0500

    added delete trigger to avoid google appscript errors

--- src/google-appscript/index.js
+++ src/google-appscript/index.js
@@ -1,5 +1,6 @@
 function sendEmailsToDiscordMain() {
   sendEmailsToDiscord();
+  deleteTrigger();
   createTriggerAfter30Sec();
 }
 
--- src/google-appscript/trigger.js
+++ src/google-appscript/trigger.js
@@ -4,3 +4,11 @@ function createTriggerAfter30Sec() {
         .after(1000 * 30)
         .create();
 }
+function deleteTrigger() {
+    const triggers = ScriptApp.getProjectTriggers();
+    for (let i = 0; i < triggers.length; i++) {
+        if (triggers[i].getHandlerFunction() == 'sendEmailsToDiscord') {
+            ScriptApp.deleteTrigger(triggers[i]);
+        }
+    }
+}
